7c6faf6aa626db63fc33aba76c8ada21110e4f13
Atharv317 <atharvshukla315@gmail.com>
2025-08-24 11:18:57 +0530

    Fix: use index.py as entrypoint for Vercel

--- index.py
+++ index.py
@@ -2,5 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "InventoryPro.settings")
-
-app = get_wsgi_application()
+app = get_wsgi_application()   # 👈 Vercel ko yahi chahiye
